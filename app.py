906f19ccf89b56e4d29adff6f149c22f89855d35
whizzer0 <whizzerZero@gmail.com>
2017-12-18 14:40:49 +0000

    Some more registration stuff:

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
+import join
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/register", methods=["GET"])
-def example():
-	return render_template("register.html", data=None)
+@app.route('/register', methods=['GET'])
+def pageGetRegister():
+	return render_template('register.html')
 
-#@app.route("/results", methods=["POST"])
-#def results():
-#    marked = processResults(questions, [request.form["question1"], request.form["question2"], request.form["question3"], request.form["question4"], request.form["question5"]])
-#    return render_template("results.html", results=[request.form["question1"], request.form["question2"], request.form["question3"], request.form["question4"], request.form["question5"]], marked=marked, questions=questions)
+@app.route('/results', methods=['POST'])
+def pagePostRegisterSuccess():
+    userData = {'name':request.form['name'], 'home':request.form['home'], 'dob':request.form['dob'], 'sex':request.form['sex'], 'genre1':request.form['genre1'], 'genre2':request.form['genre2'], 'genre3':request.form['genre3']}
+    #join.userAdd(userData)
+    return render_template('register-success.html', username=userData['name'])
 	
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run()
