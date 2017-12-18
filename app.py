a1034b01e7300d5925ce314bfb90bc48179f7fce
whizzer0 <whizzerZero@gmail.com>
2017-12-18 15:49:11 +0000

    Finally got the validation fail page to work

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request
 import join
 
+userDataBlank = {'uname':"",
+                'password':"",
+                'name':("", ""),
+                'postcode':"",
+                'birthdate':"",
+                'gender':"",
+                'genre1':True,
+                'genre2':False,
+                'genre3':False}
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/register', methods=['GET'])
 def pageGetRegister():
-	return render_template('register.html')
+	return render_template('register.html', userData=userDataBlank, error=False)
 
-@app.route('/results', methods=['POST'])
-def pagePostRegisterSuccess():
-    userData = {'name':request.form['name'], 'home':request.form['home'], 'dob':request.form['dob'], 'sex':request.form['sex'], 'genre1':request.form['genre1'], 'genre2':request.form['genre2'], 'genre3':request.form['genre3']}
-    #join.userAdd(userData)
-    return render_template('register-success.html', username=userData['name'])
+@app.route('/register', methods=['POST'])
+def pagePostRegister():
+    userData = request.form.to_dict()
+    print(userData)
+    userData['genre1'] = userData.get('genre1', False)
+    userData['genre2'] = userData.get('genre2', False)
+    userData['genre3'] = userData.get('genre3', False)
+    print(userData)
+    userData.update(join.selectedGender(userData))
+    userData.update(join.selectedGenres(userData))
+    print(userData)
+    #if join.passValidate(userData['pass']):
+        #join.userAdd(userData)
+    if False:
+        return render_template('register-success.html', userData=userData)
+    else:
+        return render_template('register.html', userData=userData, error=True)
 	
 if __name__ == '__main__':
 	app.run()
--- join.py
+++ join.py
@@ -1,3 +1,17 @@
 #Account creation
 
 from data import *
+
+def selectedGender(userData):
+    o = {'male':'', 'female':'', 'other':''}
+    o[userData['gender']] = 'selected'
+    return o
+
+def selectedGenres(userData):
+    genres = {'genre1':userData['genre1'], 'genre2':userData['genre2'], 'genre3':userData['genre3']}
+    for key, item in genres.items():
+        if item == 'on':
+            genres[key] = 'checked'
+        else:
+            genres[key] = ''
+    return genres
