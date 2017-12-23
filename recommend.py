21bcc03ed6c740dbea508d24e5f581e3f7cd823e
whizzer0 <whizzerZero@gmail.com>
2017-12-23 14:07:09 +0000

    Something

--- recommend.py
+++ recommend.py
@@ -2,7 +2,7 @@
 
 import data
 
-def count(user):
+def genreCount(user): #Determine how preferred each genre is for a particular user.
     userData = data.accountsRecall(user) #Get this user's data from the database.
     films = data.filmsRecall() #Get the films database. A bad idea if there were lots of films, but fine for our 12.
     genrePoints = {'genre1':0, 'genre2':0, 'genre3':0} #Create the dict we'll use to assign points to genres. Flaw: not extensible.
@@ -12,3 +12,16 @@ def count(user):
         else:
             for g in films[film]['genres'].split(','): #This gets us that film's genre(s) as a list and iterates over that list.
                 genrePoints[g] += 1
+    for g in userData['selectedgenres'].split(','): #Now also take into account the genres the user chose when they first registered.
+        genrePoints[g] += 1
+    return genrePoints
+
+def filmSort(genrePoints): #Sort films based on the genre scoring.
+    films = data.filmsRecall()
+    genreSort = sorted(genrePoints, key=genrePoints.get).reverse()
+    filmsSort = []
+    for genre in genreSort:
+        for film, i in films.items():
+            if genre in i['genres'] and not film in filmsSort:
+                filmsSort.append(film)
+    return filmsSort
